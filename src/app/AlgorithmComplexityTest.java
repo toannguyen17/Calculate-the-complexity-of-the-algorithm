02160d2bb993b19b67ef25abcf44495cddaf5473
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-14 15:02:40 +0700

    pay homeword

---
+++ src/app/AlgorithmComplexityTest.java
@@ -0,0 +1,47 @@
+package app;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a string:");
+		String inputString = scanner.nextLine();
+		/* codes below here */
+
+		// Thời gian thực thi thuật toán
+		Date time = new Date();
+		int[] frequentChar = new int[255];
+		Date timeCount = new Date();
+		System.out.println(timeCount.getTime()- time.getTime());
+		time = timeCount;
+
+		for (int i = 0; i < inputString.length(); i++) {
+			/* Tìm ví trí tương ứng của ký tự trong bảng ASCII */
+			int ascii = (int) inputString.charAt(i);
+			/* Tăng giá trị tần suất */
+			frequentChar[ascii] += 1;
+		}
+
+		// Thời gian thực thi thuật toán
+		timeCount = new Date();
+		System.out.println(timeCount.getTime()- time.getTime());
+		time = timeCount;
+
+		int max = 0;
+		char character = (char) 255; /* empty character */
+		for (int j = 0; j < 255; j++) {
+			if (frequentChar[j] > max) {
+				max = frequentChar[j];
+				character = (char) j;
+			}
+		}
+
+		// Thời gian thực thi thuật toán
+		timeCount = new Date();
+		System.out.println(timeCount.getTime()- time.getTime());
+
+		System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+	}
+}
